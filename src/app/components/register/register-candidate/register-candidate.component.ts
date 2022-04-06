d08777d02b252aa5bf828054db91a3be3906aced
HtewechIheb <ihebhtewech@gmail.com>
2022-04-07 00:11:56 +0100

    Added form model to register company component + Modified styles for register components

--- src/app/components/register/register-candidate/register-candidate.component.ts
+++ src/app/components/register/register-candidate/register-candidate.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { faBuilding, faAt, faLock, faPhoneAlt, faMapMarkedAlt, faUpload, faTag, faVenusMars, faIdCardAlt, faFileInvoice, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -6,7 +7,7 @@ import { faBuilding, faAt, faLock, faPhoneAlt, faMapMarkedAlt, faUpload, faTag, 
   templateUrl: './register-candidate.component.html',
   styleUrls: ['./register-candidate.component.css'],
 })
-export class RegisterCandidateComponent {
+export class RegisterCandidateComponent implements OnInit {
   public readonly faBuilding = faBuilding;
   public readonly faAt = faAt;
   public readonly faTag = faTag;
@@ -19,8 +20,9 @@ export class RegisterCandidateComponent {
   public readonly faFileInvoice = faFileInvoice;
   public readonly faCalendarAlt = faCalendarAlt;
 
-  public tos: boolean = false;
-  public gender: string;
+  public form: FormGroup;
+  public submitted: boolean;
+  public loading: boolean;
 
   @ViewChild('uploadPhotoElement') uploadPhotoElement: ElementRef;
   @ViewChild('uploadResumeElement') uploadResumeElement: ElementRef;
@@ -33,7 +35,44 @@ export class RegisterCandidateComponent {
     this.uploadResumeElement.nativeElement.click();
   }
 
-  onSubmit(): void {
+  get formControls() {
+    return this.form.controls;
+  }
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: [''],
+      address: ['', Validators.required],
+      birthdate: ['', Validators.required],
+      gender: ['', Validators.required],
+      tos: ['', [Validators.required, Validators.pattern(/true/)]],
+      resumeFile: ['', Validators.required],
+      contactNumber: [''],
+      photoFile: ['']
+    }, { validators: this.passwordsMatch });
+  }
+
+  private passwordsMatch(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    return password.value != confirmPassword.value ? { passwordsMatch: true } : null;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    this.loading = true;
+
+    if(!this.form.valid){
+      this.loading = false;
+      return;
+    }
+    console.log(this.form);
   }
 }
