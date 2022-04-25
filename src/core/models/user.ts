4c7a7ca9a03f150921fc3774502045f2b0226a00
HtewechIheb <ihebhtewech@gmail.com>
2022-04-25 14:46:03 +0100

    Fixed user model wrong id type

--- src/core/models/user.ts
+++ src/core/models/user.ts
@@ -3,7 +3,7 @@ import { Company } from "./company";
 import { UserType } from "../utilities/types";
 
 export interface User {
-  id: string,
+  id: number,
   email: string,
   type: UserType,
   info: Company | Candidate
