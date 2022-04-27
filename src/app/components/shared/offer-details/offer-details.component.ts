11d9a932321183593ca756add6745f05809cea84
HtewechIheb <ihebhtewech@gmail.com>
2022-04-27 17:02:29 +0100

    Added model to offer details page

--- src/app/components/shared/offer-details/offer-details.component.ts
+++ src/app/components/shared/offer-details/offer-details.component.ts
@@ -1,21 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { faBuilding, faClock, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
+import { Company } from 'src/core/models/company';
+import { Offer } from 'src/core/models/offer';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-offer-details',
   templateUrl: './offer-details.component.html',
   styleUrls: ['./offer-details.component.css']
 })
-export class OfferDetailsComponent implements OnInit {
-  @Input() preview: boolean;
+export class OfferDetailsComponent {
+  @Input() offer: Offer;
+  @Input() company: Company;
+  @Input() companyLogoURL: string;
 
-  faClock = faClock;
-  faBuilding = faBuilding;
-  faMapMarkedAlt = faMapMarkedAlt;
+  public readonly faClock = faClock;
+  public readonly faBuilding = faBuilding;
+  public readonly faMapMarkedAlt = faMapMarkedAlt;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
